6fcfe139b789c11ed137d4b0ffe71811d6fb5b9e
manishkumar2023 <manishkumardevelops17.com>
2026-05-03 01:38:43 +0530

    added sitemap.xml and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://xerodirt.com',
+    generateRobotsTxt: true, // also creates robots.txt
+    sitemapSize: 7000,
+};
