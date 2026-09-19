ac8c278a9aed9a90b2d42e8344db3093cdbda253
manishkumar2023 <97793295+manishkumar2023@users.noreply.github.com>
2026-09-20 00:02:54 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,6 @@ export const metadata = {
   },
 }
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -35,6 +34,50 @@ export default function RootLayout({ children }) {
           <Footer />
           <WhatsAppButton />
           <RevealOnScroll />
+        </CartProvider>
+
+        {/* Google tag (gtag.js) */}
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18462388198" 
+          strategy="afterInteractive" 
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18462388198');
+          `}
+        </Script>
+
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '4293199580939619');
+          fbq('track', 'PageView');
+        `}}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=4293199580939619&ev=PageView&noscript=1" />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
+      </body>
+    </html>
+  );
+}
+<RevealOnScroll />
         </CartProvider>
 
         {/* Meta Pixel Code */}
